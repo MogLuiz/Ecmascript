0202eaf46caa1c5243edcf9ab0d7b3163fe9f872
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-04-24 15:15:19 -0300

    Add files via upload

--- callback.js
+++ callback.js
@@ -7,11 +7,19 @@ function enviarEmail(body, sender, callback) {
             --------------------
             De: Luiz Henrique
         `)
-        callback()
+        callback("OK", 5, "8s")
     }, 8000)
 }
 
-enviarEmail("Olá , seja bem vindo !", "luizhjramos@outlook.com", () => {
+enviarEmail("Olá , seja bem vindo !", "luizhjramos@outlook.com", (status, amount, time) => {
+    console.log(`
+        Status: ${status}
+        --------------------
+        Contatos: ${amount}
+        --------------------
+        Tempo de envio: ${time}
+
+    `)
     console.log("Seu email foi enviado com sucesso!")
     console.log("Tudo Ok!!")
 })
